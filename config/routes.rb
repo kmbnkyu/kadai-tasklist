c5c95df92ee82a3ca85d12bbf08dfa385d179885
kmbnkyu <kmogksy353688@gmail.com>
2022-05-25 11:04:18 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
   #root to: 'tasks#index'
-  resources :tasks
+  #resources :tasks
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
