f061d9db4ca91838f8029b56f3392c0bf098c493
kmbnkyu <kmogksy353688@gmail.com>
2022-05-28 10:30:48 +0000

    user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:show]
   
   def index
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
