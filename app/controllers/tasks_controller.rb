eb7b1ee35bda5fcbefbfee687d8b5eea815dc5e8
kmbnkyu <kmogksy353688@gmail.com>
2022-05-28 02:15:03 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,9 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:new, :create, :update, :destroy]
+  before_action :require_user_logged_in
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    if logged_in?
     @pagy, @tasks = pagy(current_user.tasks.order(id: :desc), items: 10)
-    end
   end
 
   def show
